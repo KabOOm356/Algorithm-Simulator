f5669f1e4e7589b5da7012859328066dda3004e3
Robert Wagner <kaboom356@gmail.com>
2012-02-01 16:27:17 -0600

    Created a project entry page and corrected the spelling error of the 'Hashing Algorithm' folder

--- index.php
+++ index.php
@@ -4,4 +4,42 @@
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <title>Algorithm Simulator</title>
 </head>
+<body bgcolor="gray">
+<table border='1' cellpadding='20' align='center'>
+	<tr>
+		<td align='center'>
+			<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+				<h1>Algorithm Simulator</h1>
+				<p>
+					Pleases select which algorithm you would like to run:
+					<select name="algorithm">
+						<option value="BinSim">Binary Search Simulator</option>
+						<option value="HashSim">Hashing Simulator</option>
+						<option value="SelSort">Selection Sort Simulator</option>
+					</select>
+					<br/>
+					<input type="submit" name="Submit">
+				</p>
+			</form>
+		</td>
+	</tr>
+</table>
+<?php
+if(isset($_POST['Submit']))
+{
+	switch($_POST['algorithm'])
+	{
+		case "BinSim":
+			header("Location: Binary Search Algorithm");
+		break;
+		case "HashSim":
+			header("Location: Hashing Algorithm");
+		break;
+		case "SelSort":
+			header("Location: Selection Sort Algorithm");
+		break;
+	}
+}
+?>
+</body>
 </html>
