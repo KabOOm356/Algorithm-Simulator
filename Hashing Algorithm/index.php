0254994eb4ed6080658e05a288c26cc66464cb35
Robert Wagner <kaboom356@gmail.com>
2012-03-28 14:47:12 -0500

    Changed name of the hash_old.cgi to hash_old.php and began the initial form for the hashing simulator

--- Hashing Algorithm/index.php
+++ Hashing Algorithm/index.php
@@ -6,13 +6,28 @@
 </head>
 <body bgcolor="gray">
 <table border='1' cellpadding='20' align='center'>
-	<tr>
-		<td align='center'>
-			<?php
-				echo "<p><h1><font color='yellow'>This is still a work in progress!</font></h1></p>";
-			?>
-		</td>
-	</tr>
+<tr>
+<td align='center'>
+<h1><font color='yellow'>This is still a work in progress!</font></h1>
+<h1>Introduction to the Hashing Algorithm Simulator</h1>
+<?php
+
+echo '<p>This file was last updated: ' . date ('F d Y H:i:s.', getlastmod()) . "</p>";
+
+if(!isset($_POST['submit']))
+{
+	echo "<form method=\"POST\" action=\"" . $_SERVER['PHP_SELF'] . "\">";
+	echo "Indicate your initial Array Size:  <input type=\"radio\" name=\"Size\" value=\"7\" checked />";
+	echo "7  ";
+	echo "<input type=\"radio\" name=\"Size\" value=\"12\" /> 12 <input type=\"radio\" name=\"Size\" value=\"15\" /> 15<br /><br />";
+	echo "Input Range: <input name=\"range1\" value=\"$range1\" size=\"3\" maxlength=\"3\" /> to <input name=\"range2\" value=\"$range2\" size=\"3\" maxlength=\"3\" />";
+	echo "<br /><br />";
+	echo "<input type=\"submit\" name=\"Send\" value=\"Run Simulator\" /> <input type=\"reset\" value=\"Reset\">";
+	echo "</form>";
+}
+?>
+</td>
+</tr>
 </table>
 </body>
 </html>
