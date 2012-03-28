320bebb4579b6a429b12820036552caaac9d6065
Robert Wagner <kaboom356@gmail.com>
2012-03-28 16:01:08 -0500

    Added input persistence and a input summary screen

--- Hashing Algorithm/index.php
+++ Hashing Algorithm/index.php
@@ -9,21 +9,105 @@
 <tr>
 <td align='center'>
 <h1><font color='yellow'>This is still a work in progress!</font></h1>
-<h1>Introduction to the Hashing Algorithm Simulator</h1>
 <?php
 
-echo '<p>This file was last updated: ' . date ('F d Y H:i:s.', getlastmod()) . "</p>";
+// TODO remove this after debugging
+ini_set("display_errors", 1);
 
-if(!isset($_POST['submit']))
+$start = false;
+
+if(isset($_POST['submit']))
 {
+	$start = true;
+	
+	echo "<p><font color=red>";
+	
+	if(empty($_POST['range1']))
+	{
+		$start = false;
+		echo "Lower bound is not set!<br />";
+	}
+	elseif(!is_numeric($_POST['range1']))
+	{
+		$start = false;
+		echo "Lower bound is not an integer!<br />";
+	}
+	
+	if(empty($_POST['range2']))
+	{
+		$start = false;
+		echo "Upper bound is not set!<br />";
+	}
+	elseif(!is_numeric($_POST['range2']))
+	{
+		$start = false;
+		echo "Upper bound is not an integer!<br />";
+	}
+	
+	// Check if there were any errors with the user input
+	if(!$start)
+		echo "<b>Please review the selections below and resubmit the form</b>";
+	
+	echo "</font></p>";
+}
+
+if(!$start)
+{
+	echo "<h1>Introduction to the Hashing Algorithm Simulator</h1>";
+	
+	echo '<p>This file was last updated: ' . date ('F d Y H:i:s.', getlastmod()) . "</p>";
+	
+	// TODO write a description
+	echo "<p><i>Write a description...</i></p>";
+	
 	echo "<form method=\"POST\" action=\"" . $_SERVER['PHP_SELF'] . "\">";
-	echo "Indicate your initial Array Size:  <input type=\"radio\" name=\"Size\" value=\"7\" checked />";
-	echo "7  ";
-	echo "<input type=\"radio\" name=\"Size\" value=\"12\" /> 12 <input type=\"radio\" name=\"Size\" value=\"15\" /> 15<br /><br />";
+	
+	echo "Indicate your initial Array Size:  ";
+	
+	if(empty($_POST['Size']))
+		$_POST['Size'] = -1;
+	
+	switch ($_POST['Size'])
+	{
+			case 7:
+				echo "<input type='radio' name='Size' value='7' Checked> 7 ";
+				echo "<input type='radio' name='Size' value='12'> 12 ";
+				echo "<input type='radio' name='Size' value='15'> 15<br/><br/>";
+				break;
+			case 15:
+				echo "<input type='radio' name='Size' value='7'> 7 ";
+				echo "<input type='radio' name='Size' value='12'> 12 ";
+				echo "<input type='radio' name='Size' value='15' Checked> 15<br/><br/>";
+				break;
+			default:
+				echo "<input type='radio' name='Size' value='7'> 7 ";
+				echo "<input type='radio' name='Size' value='12' Checked> 12 ";
+				echo "<input type='radio' name='Size' value='15'> 15<br/><br/>";
+				break;
+	}
+	
+	$range1 = (!empty($_POST['range1'])) ? $_POST['range1'] : "";
+	$range2 = (!empty($_POST['range2'])) ? $_POST['range2'] : "";
+	
 	echo "Input Range: <input name=\"range1\" value=\"$range1\" size=\"3\" maxlength=\"3\" /> to <input name=\"range2\" value=\"$range2\" size=\"3\" maxlength=\"3\" />";
 	echo "<br /><br />";
-	echo "<input type=\"submit\" name=\"Send\" value=\"Run Simulator\" /> <input type=\"reset\" value=\"Reset\">";
+	echo "<input type=\"submit\" name=\"submit\" value=\"Run Simulator\" /> <input type=\"reset\" value=\"Reset\"><br />";
 	echo "</form>";
+	
+	// Form to return to the main 
+	echo "<form action='../' method='POST'>";
+	echo "<input type='submit' value='Return to Simulation Selection'>";
+	echo "</form>";
+}
+else
+{
+	echo "<h1>Input Summary</h1>";
+	
+	echo "<p>";
+	echo "Size: " . $_POST['Size'] . "<br/>";
+	echo "Lower Bound: " . $_POST['range1'] . "<br/>";
+	echo "Upper Bound: " . $_POST['range2'] . "<br/>";
+	echo "</p>";
 }
 ?>
 </td>
