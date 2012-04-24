7b92fbf0ff0cb5156c46ab5ad94c6d25eeabcddf
nick <nick@nick-PC>
2012-04-23 23:26:14 -0500

    Started bubble sort logic. Need to work out some kinks because the array doesn't come out sorted right. After that, I will add the written section.

---
+++ bubble sort/bubble_sort.php
@@ -0,0 +1,259 @@
+<?php
+set_up_values();
+
+/*debug to make sure logic works
+$array_copy = unserialize($_POST['array']);
+print_r($array_copy);
+echo "<br />";
+bubbleSort($array_copy);
+*/
+//this is the bubble sort logic I will be using
+function bubbleSort($array)
+{
+    if (!$length = count($array))
+    {
+        return $array;
+    }
+    
+    //$length = count($array);
+    
+    for ($outer = 0; $outer < $length; $outer++)
+    {
+        for ($inner = 0; $inner < $length; $inner++)
+        {
+            if ($array[$outer] < $array[$inner])
+            {
+                $tmp = $array[$outer];
+                $array[$outer] = $array[$inner];
+                $array[$inner] = $tmp;
+            }
+        }
+    }
+    print_r($array);
+}
+function show_algorithm($which_line){
+            //insertion sort logic
+        
+    /*
+    [0]
+    for ($outerVal = 0; $outerVal < $length; $outerVal++)
+    {
+       [1]
+       for ($innerVal = 0; $innerVal < $length; $innerVal++)
+        {
+            [2]
+            if ($array_copy[$outerVal] < $array_copy[$innerVal])
+            {
+               [3]
+                $tmpVal = $array_copy[$outerVal];
+                $array_copy[$outerVal] = $array_copy[$innerVal];
+                $array_copy[$innerVal] = $tmpVal;
+            }
+        }
+    }
+    */
+    
+        echo '<div align="right">';
+        echo '<table border = "1">';
+        echo '<tr><th bgcolor="#808080">What Line of the Sort Are We In</th></tr>';
+        //the value of $which_line will show which line of the algorithm we are at by making the
+        //background of that line yellow
+        if($which_line==0)
+        {
+            echo '<tr><td bgcolor="#FFFF00">for ($outerVal = 0; $outerVal < $length; $outerVal++)</td></tr>';
+            echo '<tr><td>{</td></tr>';
+        }else
+        {
+            echo '<tr><td>for ($outerVal = 0; $outerVal < $length; $outerVal++)</td></tr>';
+            echo '<tr><td>{</td></tr>';
+        }
+        
+        if($which_line==1)
+        {
+            echo '<tr><td bgcolor="#FFFF00">&nbsp;&nbsp;&nbsp;&nbsp;for ($innerVal = 0; $innerVal < $length; $innerVal++)</td></tr>';
+            echo '<tr><td>&nbsp;&nbsp;&nbsp;&nbsp;{</td></tr>';
+        }else
+        {
+            echo '<tr><td>&nbsp;&nbsp;&nbsp;&nbsp;for ($innerVal = 0; $innerVal < $length; $innerVal++)</td></tr>';
+            echo '<tr><td>&nbsp;&nbsp;&nbsp;&nbsp;{</td></tr>';
+        }
+        
+        if($which_line==2)
+        {
+             echo '<tr><td bgcolor="#FFFF00">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;if ($array_copy[$outerVal] < $array_copy[$innerVal])</td></tr>';
+             echo '<tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{</td></tr>';
+        }else
+        {
+            echo '<tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;if ($array_copy[$outerVal] < $array_copy[$innerVal])</td></tr>';
+            echo '<tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{</td></tr>';
+        }
+        if($which_line==3)
+        {
+            echo '<tr><td bgcolor="#FFFF00">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$tmpVal = $array_copy[$outerVal];</td></tr>';
+        }else
+        {
+            echo '<tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$tmpVal = $array_copy[$outerVal];</td></tr>';
+        }
+        if($which_line==3)
+        {
+            echo '<tr><td bgcolor="#FFFF00">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$array_copy[$outerVal] = $array_copy[$innerVal];</td></tr>';
+        }else
+        {
+            echo '<tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$array_copy[$outerVal] = $array_copy[$innerVal];</td></tr>';
+        }
+        if($which_line==3)
+        {
+            echo '<tr><td bgcolor="#FFFF00">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$array_copy[$innerVal] = $tmpVal;</td></tr>';
+            
+        }else
+        {
+            echo '<tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$array_copy[$innerVal] = $tmpVal;</td></tr>';
+        }
+        
+        echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;}';
+        echo '&nbsp;&nbsp;&nbsp;&nbsp;}';
+        echo '}';
+        
+        echo '</table>';
+        echo '</div>';
+        echo "<br />";
+        
+        
+    }
+function set_up_values(){
+    $array_copy = unserialize($_POST['array']);
+    $lineNum = $_POST['lineNum'];
+    $outerVal = $_POST['outerVal']; 
+    $innerVal = $_POST['innerVal'];
+    $tmpVal = $_POST['tmpVal'];
+    //$length = count($array1);
+    
+    echo "DEBUG STUFF <br />";
+    echo "line num: $lineNum <br />\n";
+    echo "outerval: $outerVal <br />\n";
+    echo "innerval: $innerVal <br />\n";
+    echo "tmpval: $tmpVal <br />\n";
+    
+    bubble_sort($array_copy,$lineNum,$outerVal,$innerVal,$tmpVal);
+
+}
+
+function bubble_sort($array1,$lineNum1,$outerVal1,$innerVal1, $tmpVal1){
+    $array_copy = $array1;
+    $lineNum = $lineNum1;
+    $outerVal = $outerVal1;
+    $innerVal = $innerVal1;
+    $tmpVal = $tmpVal1;
+    $length = count($array_copy);
+    /*
+    [0]for ($outerVal = 0; $outerVal < $length; $outerVal++)
+    {
+       [1] for ($innerVal = 0; $innerVal < $length; $innerVal++)
+        {
+            [2]if ($array_copy[$outerVal] < $array_copy[$innerVal])
+            {
+               [3] $tmpVal = $array_copy[$outerVal];
+                $array_copy[$outerVal] = $array_copy[$innerVal];
+                $array_copy[$innerVal] = $tmpVal;
+            }
+        }
+    }
+    */
+    
+    show_algorithm($lineNum);
+    switch($lineNum)
+    {
+        /*
+        Line number = 0
+        for ($outerVal = 0; $outerVal < $length; $outerVal++)
+        */
+        
+        case 0:
+            if($outerVal < $length){
+                //enter for-loop
+                $lineNum =1;
+            }else{
+                $lineNum = 4;
+                //algorithm is over
+            }
+            next_step_button($array_copy,$lineNum,$outerVal,$innerVal,$tmpVal);
+        break;
+        /*
+        Line number = 1
+        for ($innerVal = 0; $innerVal < $length; $innerVal++)
+        */
+        case 1:    
+            if($innerVal < $length){
+                //enter for-loop
+                $lineNum =2;
+            }else{
+                //algorithm is over
+                $outerVal = $outerVal + 1;
+                $lineNum = 0;
+            }
+            next_step_button($array_copy,$lineNum,$outerVal,$innerVal,$tmpVal);
+        break;
+        /*
+        Line number = 2
+        if ($array_copy[$outerVal] < $array_copy[$innerVal])
+        */
+        case 2:
+            $compareValue1 = $array_copy[$outerVal]; 
+            $compareValue2 = $array_copy[$innerVal];
+            if($compareValue1<$compareValue2){
+                //enter if-statement
+                $lineNum =3;
+            }else{
+                //pass over if-statement
+                $innerVal = $innerVal + 1;
+                $lineNum = 1;
+            }
+            
+            next_step_button($array_copy,$lineNum,$outerVal,$innerVal,$tmpVal);
+        break;
+        /*
+        Line number = 3
+        $tmpVal = $array_copy[$outerVal];
+        $array_copy[$outerVal] = $array_copy[$innerVal];
+        $array_copy[$innerVal] = $tmpVal;
+        */
+        case 3:
+            
+            $tmpVal = $array_copy[$outerVal];
+            $array_copy[$outerVal] = $array_copy[$innerVal];
+            $array_copy[$innerVal] = $tmpVal;
+            
+            $innerVal = $innerVal + 1;
+            $lineNum = 1;
+            next_step_button($array_copy,$lineNum,$outerVal,$innerVal,$tmpVal);
+            //increment inner for-loop
+        break;
+        /*
+        Line number = 4
+        case for when algorithm is over
+        */
+        case 4:
+            print_r($array_copy);
+            echo "<br /> ARRAY SHOULD BE SORTED!";
+        break;
+        
+    }
+        
+}
+function next_step_button($array_copy, $lineNum, $outerVal, $innerVal, $tmpVal){
+    /*variables needed
+    array, linenum, outerval, innerval, tmpVal, length
+    */
+    // Form to continue to the next step
+    echo "<form name='form' action='" . $_SERVER['PHP_SELF'] . "' method='POST'>\n";
+    echo "<input type='hidden' name='array' value='" . serialize($array_copy) . "'>\n";
+    echo "<input type='hidden' name='outerVal' value='$outerVal'>\n";
+    echo "<input type='hidden' name='innerVal' value='$innerVal'>\n";
+    echo "<input type='hidden' name='lineNum' value='$lineNum'>\n";
+    echo "<input type='hidden' name='tmpVal' value='$tmpVal'>\n";
+    echo "<input type=\"hidden\" name=\"hasPrevious\" value=\"true\">\n";
+    echo "<input type='submit' value=\"Next Step in Simulation\">\n";
+    echo "</form>\n";
+}
+
+?>
---
+++ bubble sort/index.php
@@ -0,0 +1,123 @@
+<html>
+<head><title>Software Development II</title></head>
+<body>
+
+<div id= "menu">
+    
+<?php
+    echo "<form action='" . $_SERVER['PHP_SELF'] . "' method='POST'>\n";
+    echo "<table>\n";
+    echo "<tr>\n";
+    echo "<td>Array Size:</td>\n";
+    echo "<td>\n";
+    echo "<input type=\"radio\" name=\"arraySize\" value=\"7\" /> 7 <input type=\"radio\" name=\"arraySize\" value=\"12\" />12<input type=\"radio\" name=\"arraySize\" value=\"17\" />17\n";
+    echo "</td>\n";
+    echo "</tr>\n";
+    echo "<tr>\n";
+    echo "<td>\n";
+    echo "<input type=\"radio\" name=\"arrayType\" value=\"random\"/>    Random numbers in this range: \n";
+    echo "</td>\n";
+    echo "<td>\n";
+    if(isset($_POST['rangeLow'])&&isset($_POST['rangeHigh'])){
+                 
+        echo "<input type =\"text\" name=\"rangeLow\" id=\"rangeLow\" size=\"5\" maxlength =\"10\" value = \"".$_POST['rangeLow']."\"/> to <input type =\"text\" name=\"rangeHigh\" id=\"rangeHigh\" size=\"5\" maxlength =\"10\" value = \"".$_POST['rangeHigh']."\"/>\n";    
+            
+    }elseif(isset($_POST['rangeLow'])){
+        echo "Set a high range!\n";
+        echo "<input type =\"text\" name=\"rangeLow\" id=\"rangeLow\" size=\"5\" maxlength =\"10\" value = \"".$_POST['rangeLow']."\"/> to <input type =\"text\" name=\"rangeHigh\" id=\"rangeHigh\" size=\"5\" maxlength =\"10\"/>\n";
+            
+    }elseif(isset($_POST['rangeHigh'])){
+        echo "Set a low range!\n";
+        echo "<input type =\"text\" name=\"rangeLow\" id=\"rangeLow\" size=\"5\" maxlength =\"10\"/> to <input type =\"text\" name=\"rangeHigh\" id=\"rangeHigh\" size=\"5\" maxlength =\"10\" value = \"".$_POST['rangeHigh']."\"/>\n"; 
+            
+    }elseif(!isset($_POST['rangeLow'])&&!isset($_POST['rangeHigh'])){
+        echo "Set both ranges!\n";
+        echo "<input type =\"text\" name=\"rangeLow\" id=\"rangeLow\" size=\"5\" maxlength =\"10\"/> to <input type =\"text\" name=\"rangeHigh\" id=\"rangeHigh\" size=\"5\" maxlength =\"10\"/>\n";        
+    }           
+ 
+    echo "</td>\n";
+    echo "</tr>\n";
+    echo "<tr>\n";
+    echo "<td>\n";
+    
+    echo "<input type=\"radio\" name=\"arrayType\" value=\"userVals\" /> Indexes in Array (number followed by a space):\n";            
+
+    echo "</td>\n";
+    echo "<td>\n";
+    if(isset($_POST['arrayIndexes'])){
+        echo "<input type =\"text\" name=\"arrayIndexes\" id=\"arrayIndexes\" size=\"50\" maxlength =\"70\" value =\"".$_POST['arrayIndexes']."\"/>\n";            
+    }else{
+        echo "<input type =\"text\" name=\"arrayIndexes\" id=\"arrayIndexes\" size=\"50\" maxlength =\"70\"/>\n";
+    }
+    echo "</td>\n";
+    echo "<td>\n";
+    echo "Ex: \"1 13 14 15 87 99\"\n";
+    echo "</td>\n";
+    echo "<tr>\n";
+    echo "<td>\n";
+    echo "<br /><input type=\"submit\" name = \"submit\" value=\"Start\" id=\"button1\" />\n";
+    echo "</td>\n";
+    echo "</tr>\n";
+    echo "</table>\n";
+    echo "</form>\n";
+    echo "ONLY USE THE RANDOM NUMBER RANGE SELCTION AS OF NOW, I NEED TO FIX THE USER MADE ARRAY PART!!";
+        
+            //logic to create array
+    if(isset($_POST['arraySize']))
+    {
+        if(is_numeric($_POST['rangeLow'])&&is_numeric($_POST['rangeHigh']))
+        {                
+            $array1 = makeArray();
+            go_to_start($array1);
+        }else
+        {
+            echo "One of your ranges is not numeric!\n";
+        }
+    }elseif(isset($_POST['arraySize']))
+    {
+        if(isset($_POST['arrayIndexes']))
+        {
+            $array1 = makeArray();
+            go_to_start($array1);
+        }else
+        {
+            echo "If you wanted to create your own indexes they were not set!\n";
+        }
+    }
+            
+    function makeArray()
+    {
+        $var_name = $_POST['arrayType'];
+        
+        if($var_name=="random")
+        {
+            for($i = 0; $i < $_POST['arraySize']; $i++)
+                $array[$i] = rand($_POST['rangeLow'], $_POST['rangeHigh']);
+                               
+                shuffle($array);
+        }elseif($var_name=="userVals")
+        {
+            // Split the string array by spaces
+            $array = explode(" ", $_POST['arrayIndexes']);
+                                    
+            for($i = 0; $i < count($array); $i++)
+            $array[$i] = intval($array[$i]);
+                                                
+        }
+                        
+        return $array;
+    }
+    function go_to_start($array1)
+    {
+        echo "<form action='bubble_sort.php' method='POST'>\n";
+	echo "<input type='hidden' name='array' value='" . serialize($array1) . "'>\n";
+	echo "<input type='hidden' name='lineNum' value='0'>\n";
+        echo "<input type='hidden' name='outerVal' value='0'>\n";
+        echo "<input type='hidden' name='innerVal' value='0'>\n";
+        echo "<input type='hidden' name='tmpVal' value='0'>\n";
+	echo "<input type='submit' value='Run Simulation'>\n";
+	echo "</form>";
+    }
+            
+
+?>
